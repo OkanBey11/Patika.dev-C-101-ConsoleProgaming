e4d54ac4802dd5b0ce05133eeb2e225cc7fa2cfd
OkanBey11 <80157606+OkanBey11@users.noreply.github.com>
2023-04-04 19:00:43 +0300

    Add files via upload

---
+++ TernaryIfElse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time>=6 && time < 11)
+            {
+                Console.WriteLine("Günaydınlar !!");
+            }
+            else if(time <= 18)
+            {
+                Console.WriteLine("İyi Günler!!");
+            }
+            else
+            {
+                Console.WriteLine("İyi akşamlar !!");
+            }
+
+            string sonuc = time <= 18 ? "İyi Günler!!" : "İyi geceler";
+
+            sonuc = time >= 6 && time < 11 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi geceler";
+
+            Console.ReadKey();
+        }
+    }
+}
