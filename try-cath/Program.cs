c132a02608a97269222545639a643f7437124832
OkanBey11 <80157606+OkanBey11@users.noreply.github.com>
2023-04-04 13:18:08 +0300

    Add files via upload

---
+++ try-cath/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_cath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try 
+            {
+                Console.WriteLine("Bir sayi giriniz");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayı = " + sayi);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Hata :" + ex.Message.ToString());
+            }
+            finally 
+            {
+                Console.WriteLine("İşlem tamam");
+            }
+
+            try 
+            {
+                int a = int.Parse(null);
+            }
+            catch (ArgumentNullException exx) 
+            {
+                Console.WriteLine("Değer null girilmez");
+                Console.WriteLine(exx);
+            }
+            catch (FormatException ex) 
+            {
+                Console.WriteLine("Değer null girilmez");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex) 
+            {
+                Console.WriteLine("Menzil dışı");
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
