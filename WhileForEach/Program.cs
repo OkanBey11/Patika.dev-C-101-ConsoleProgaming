b050912e686c3bc5face8405fbd0e65e6ca30ecd
OkanBey11 <80157606+OkanBey11@users.noreply.github.com>
2023-04-07 14:59:45 +0300

    Add files via upload

---
+++ WhileForEach/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            //1 den başlayarak konsoldan girilen sayıya(dahil) kadar olan sayıların ortalamasını konsola bastıran program
+            Console.WriteLine("1 den kaça akdar olan sayıların ortalamasını verelrim size : ");
+            int sayi =  int.Parse(Console.ReadLine());
+            int temp = sayi;
+            int toplam = 0;
+            while (sayi >= 1)
+            {
+                toplam += sayi;
+                sayi--;
+            }
+            Console.WriteLine("Ortalamanız : " + toplam/temp);
+
+            // a dan z ye kadar olan harfleri yazma
+            char charset = 'a';
+            while(charset <= 'z')
+            {
+                Console.Write(charset);
+                charset++;
+            }
+
+            Console.WriteLine();
+            // foreach
+            string[] meyveler = {"Elma","Muz","Armut","Üzüm"};
+            foreach (var meyve in meyveler)
+            {
+                Console.WriteLine(meyve);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
