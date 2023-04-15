0006ad9ec4b0527c0075e50824243f04cd4a6358
OkanBey11 <80157606+OkanBey11@users.noreply.github.com>
2023-04-15 13:06:13 +0300

    Add files via upload

---
+++ hazir_methods_datetime_math/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hazir_methods_datetime_math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+            Console.WriteLine(DateTime.Now.Millisecond);
+
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+            Console.WriteLine(DateTime.Now.DayOfYear);
+
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+
+            Console.WriteLine(DateTime.Now.AddDays(2));
+            Console.WriteLine(DateTime.Now.AddHours(2));
+            Console.WriteLine(DateTime.Now.AddSeconds(20));
+            Console.WriteLine(DateTime.Now.AddMonths(5));
+            Console.WriteLine(DateTime.Now.AddYears(5));
+            Console.WriteLine(DateTime.Now.AddMilliseconds(5));
+
+            //Date Time Format
+            Console.WriteLine(DateTime.Now.ToString("dd"));
+            Console.WriteLine(DateTime.Now.ToString("ddd"));
+            Console.WriteLine(DateTime.Now.ToString("dddd"));
+
+            Console.WriteLine(DateTime.Now.ToString("MM"));
+            Console.WriteLine(DateTime.Now.ToString("MMM"));
+            Console.WriteLine(DateTime.Now.ToString("MMMM"));
+
+            Console.WriteLine(DateTime.Now.ToString("yy"));
+            Console.WriteLine(DateTime.Now.ToString("yyy"));
+            Console.WriteLine(DateTime.Now.ToString("yyyy"));
+
+            //Math 
+            Console.WriteLine(Math.Abs(-25));
+            Console.WriteLine(Math.Sin(30));
+            Console.WriteLine(Math.Cos(30));
+            Console.WriteLine(Math.Tan(30));
+
+            Console.WriteLine(Math.Ceiling(22.3)); //23
+            Console.WriteLine(Math.Round(22.3)); //22
+            Console.WriteLine(Math.Round(22.7)); //23
+            Console.WriteLine(Math.Floor(22.7)); //22 
+
+            Console.WriteLine(Math.Min(2,6)); //2
+            Console.WriteLine(Math.Max(2,6)); //6
+
+            Console.WriteLine(Math.Pow(3,4)); // 81
+            Console.WriteLine(Math.Sqrt(9)); //3
+            Console.WriteLine(Math.Log(10)); // log e 10 
+            Console.WriteLine(Math.Log10(10)); // log 10 tabanı 10 verir
+            Console.WriteLine(Math.Exp(10)); // e üzeri 10 verir
+
+            Console.ReadKey();
+        }
+    }
+}
