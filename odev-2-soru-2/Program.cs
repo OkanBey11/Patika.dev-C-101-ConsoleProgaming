64d129daa4b241b0791e6ac684b4a4658f648813
OkanBey11 <80157606+OkanBey11@users.noreply.github.com>
2023-04-19 20:31:05 +0300

    Add files via upload

---
+++ odev-2-soru-2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev_2_soru_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[20];
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write("Sayi giriniz: ");
+                int sayi = int.Parse(Console.ReadLine());
+                sayilar[i] = sayi;
+            }
+
+            Array.Sort(sayilar);
+
+            int toplamBuyuk = 0;
+            for (int i = 19; i >= 17; i--)
+            {
+                Console.WriteLine("En buyuk " + (20 - i) + ". sayi: " + sayilar[i]);
+                toplamBuyuk += sayilar[i];
+            }
+
+            int toplamKucuk = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("En kucuk " + (i + 1) + ". sayi: " + sayilar[i]);
+                toplamKucuk += sayilar[i];
+            }
+
+            double ortalamaBuyuk = (double)toplamBuyuk / 3;
+            double ortalamaKucuk = (double)toplamKucuk / 3;
+
+            Console.WriteLine("En buyuk sayilarin ortalamasi: " + ortalamaBuyuk);
+            Console.WriteLine("En kucuk sayilarin ortalamasi: " + ortalamaKucuk);
+            Console.WriteLine("Ortalama toplamlari: " + (ortalamaBuyuk + ortalamaKucuk));
+
+            Console.ReadKey();
+        }
+    }
+}
