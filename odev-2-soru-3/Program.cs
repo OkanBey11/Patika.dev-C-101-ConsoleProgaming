d74ba27b5bfe354a647770e2981c00e3ddc9f501
OkanBey11 <80157606+OkanBey11@users.noreply.github.com>
2023-04-19 20:31:49 +0300

    Add files via upload

---
+++ odev-2-soru-3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev_2_soru_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir cumle giriniz: ");
+            string cumle = Console.ReadLine();
+
+            char[] sesliHarfler = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            char[] cumleHarfler = cumle.ToLower().ToCharArray();
+            Array.Sort(cumleHarfler);
+
+            int sesliSayisi = 0;
+
+            Console.Write("Cumledeki sesli harfler: ");
+
+            for (int i = 0; i < cumleHarfler.Length; i++)
+            {
+                if (Array.IndexOf(sesliHarfler, cumleHarfler[i]) != -1)
+                {
+                    Console.Write(cumleHarfler[i] + " ");
+                    sesliSayisi++;
+                }
+            }
+
+            if (sesliSayisi == 0)
+            {
+                Console.WriteLine("Cumlede sesli harf bulunamadi.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
